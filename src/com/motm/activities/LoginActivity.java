b45338fe6259e8bc1c4d66c7c1416f79b29867ba
Michael Ozeryansky <mozer624@gmail.com>
2013-02-25 12:39:35 -0500

    flow

---
+++ src/com/motm/activities/LoginActivity.java
@@ -0,0 +1,77 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.motm.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import com.motm.R;
+
+/**
+ *
+ * @author michael
+ */
+public class LoginActivity extends Activity
+{
+    // models
+    
+    // view variables
+    EditText loginNameInput;
+    EditText passwordInput;
+    
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle icicle)
+    {
+        super.onCreate(icicle);
+        
+        // set the models
+        
+        
+        // setup the view elements
+        loginNameInput = (EditText)findViewById(R.id.loginNameInput);
+        passwordInput = (EditText)findViewById(R.id.passwordInput);
+        
+        // set the view
+        setContentView(R.layout.login);
+    }
+    
+    private void startRegisterActivity()
+    {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+    
+    private void returnToMainActivity()
+    {
+        finish();
+    }
+    
+    /*
+     *      Actions
+     */
+    
+    public void loginButtonPressed(View view)
+    {
+        // attempt login
+        
+        // successful
+        returnToMainActivity();
+        
+        // failure
+        // notify user
+        // clear fields
+    }
+    
+    public void registerButtonPressed(View view)
+    {
+        startRegisterActivity();
+    }
+}
---
+++ src/com/motm/activities/MainActivity.java
@@ -0,0 +1,41 @@
+package com.motm.activities;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
+import android.view.Menu;
+import com.motm.R;
+
+public class MainActivity extends Activity
+{
+
+        private static final String TAG = "MainActivity";
+    
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+                
+                // if the user is not logged in
+                if(true){
+                      // take them to the login activity
+                      startLoginActivity();
+                }
+            
+                setContentView(R.layout.main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+        
+        private void startLoginActivity()
+        {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
+
+}
