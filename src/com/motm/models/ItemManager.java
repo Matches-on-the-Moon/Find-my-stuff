4f8908d6fdbedf991d311d9b0198155301ac1f2c
Andrew <Andrew@143.215.109.218>
2013-03-04 12:31:29 -0500

    added a blank methods to Item Manager class

--- src/com/motm/models/ItemManager.java
+++ src/com/motm/models/ItemManager.java
@@ -1,9 +1,57 @@
 package com.motm.models;
 
+import java.util.Date;
+
 public class ItemManager
 {
-    public boolean createItem(String itemName, String itemLocation, String itemReward, String itemType, String itemCategory, String itemDescription)
+    public boolean createItem(int userID, Date date, String itemName, String itemLocation, String itemReward, String itemType, String itemCategory, String itemDescription)
     {
     	return true;
+    }
+    
+    public boolean deleteItem(int ItemID)
+    {
+        return true;
+    }
+    
+    public boolean editItem(int ItemID)
+    {
+        return true;
+    }
+    
+    public Item[] findItemsByUserID(int userID)
+    {
+        Item[] items = {};
+        return items;
+    }
+    
+    public Item[] findItemsByLocation(String location)
+    {
+        Item[] items = {};
+        return items;
+    }
+    
+    public Item[] findItemsByStatus(String status)
+    {
+        Item[] items = {};
+        return items;
+    }
+    
+    public Item[] findItemsByCategory(String category)
+    {
+        Item[] items = {};
+        return items;
+    }
+    
+    public Item[] findItemsByType(String type)
+    {
+        Item[] items = {};
+        return items;
+    }
+    
+    public Item[] findItemsByDate(Date date)
+    {
+        Item[] items = {};
+        return items;
     }
 }
