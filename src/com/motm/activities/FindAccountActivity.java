6d0c43b230de4f30184c844b6236a7f2c72c0f69
Will <wholton@gatech.edu>
2013-03-06 12:59:24 -0500

    Added layouts to those activities (still need buttons and what not)

--- src/com/motm/activities/FindAccountActivity.java
+++ src/com/motm/activities/FindAccountActivity.java
@@ -2,11 +2,12 @@ package com.motm.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.motm.R;
 
 public class FindAccountActivity extends Activity {
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-        // ToDo add your GUI initialization code here        
+        setContentView(R.layout.account_main);  
     }
 }
--- src/com/motm/activities/ViewAccountActivity.java
+++ src/com/motm/activities/ViewAccountActivity.java
@@ -2,11 +2,12 @@ package com.motm.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.motm.R;
 
 public class ViewAccountActivity extends Activity {
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-        // ToDo add your GUI initialization code here        
+        setContentView(R.layout.account_view);       
     }
 }
